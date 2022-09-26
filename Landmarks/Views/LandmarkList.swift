dd6668255036bc2bcd78f01b88685eaa2f59abff
Anjas Mulya Putra <anjasmulyaputra@gmail.com>
2022-09-26 13:08:17 +0700

    Filter the List View

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -8,18 +8,30 @@
 import SwiftUI
 
 struct LandmarkList: View {
-    var body: some View {
-        NavigationView {
-            List(landmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
-                }
-            }
-            .navigationTitle("Landmarks")
+    @State private var showFavoritesOnly = true
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
         }
     }
+    var body: some View {
+            NavigationView {
+                List {
+                    Toggle(isOn: $showFavoritesOnly) { Text("Favorites only")
+                    }
+                    
+                    ForEach(filteredLandmarks) { landmark in
+                        NavigationLink {
+                            LandmarkDetail(landmark: landmark)
+                        } label: {
+                            LandmarkRow(landmark: landmark)
+                        }
+                    }
+                }
+                .navigationTitle("Landmarks")
+            }
+        }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
